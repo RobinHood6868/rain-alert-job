79b4299c3e2ead7cc4122780b30fc642662ea7f7
Vũ Minh Dũng <minhdung0056@gmail.com>
2026-03-02 08:39:44 +0700

    Remove some debug code

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ from dotenv import load_dotenv
 import smtplib
 
 
+# 1. Load values from .env
 load_dotenv()
 
-
-# Load WEATHER API KEY from .env
 WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 
-# LAT, LONG (Hanoi, Viet Nam)
+# 2. Weather data for Hanoi, Viet Nam
 LAT = 21.027763
 LONG = 105.834160
 
-
-# parameters
 parameters = {
     "lat": LAT,
     "lon": LONG,
@@ -25,7 +24,7 @@ parameters = {
 }
 
 
-# Main execution
+# 3. Main execution
 API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 response = requests.get(API_ENDPOINT, params=parameters)
 response.raise_for_status()
@@ -33,15 +32,11 @@ hours_data = response.json()["list"]
 
 will_rain = False
 for data in hours_data:
-    print(data)
     condition_code = data["weather"][0]["id"]
-    print(condition_code)
     if condition_code < 700:
         will_rain = True
 
 if will_rain:
-    MY_EMAIL = os.environ.get("MY_EMAIL")
-    MY_PASSWORD = os.environ.get("MY_PASSWORD")
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
